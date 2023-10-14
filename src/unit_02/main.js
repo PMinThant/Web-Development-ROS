981027aa81232ab821c2e2d2ef421d00be61b3de
phyo <pminthant@outlook.com>
2023-10-14 12:51:25 +0700

    update unit2

---
+++ src/unit_02/main.js
@@ -0,0 +1,10 @@
+let vueApp = new Vue({
+    el: "#vueApp",
+    data:{
+        menu_title: 'My menu title',
+        main_title: 'Main title, from Vue!!',
+        paragraph_1: "Let's connect our website to a ROS robot!",
+        paragraph_2: "This is the left side of my web page. It occupies 33% of the total width",
+        paragraph_3: "Here it goes the main content of my first web page, able to control ROS robots using web tools!"
+    }
+})
